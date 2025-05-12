5c4470df4cf9bd5fb5df9cf71383e76a1e737494
iki789 <ikhlak789@gmail.com>
2025-05-12 12:37:34 -0700

    added mongoose

---
+++ backend/src/types/product.d.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+  category: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
