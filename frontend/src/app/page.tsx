1420871b8dc33d04dfcac12f3d8ce7f0e9ebc756
Tomoya <flyingfox193807@gmail.com>
2025-05-12 13:48:51 -0700

    created search and product display

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,103 +1,85 @@
-import Image from 'next/image';
+'use client';
+import { useState, useEffect } from 'react';
+import { Search } from 'lucide-react';
+import { Product } from '@/types/product';
 
-export default function Home() {
+
+const Home = () => {
+
+  const [products, setProducts] = useState<Product[]>([]);
+  const [search, setSearch] = useState<string>('');
+  const [searchInput, setSearchInput] = useState<string>('');
+  const [categories, setCategories] = useState<string[]>([])
+
+  const fetchProducts = async () => {
+    const res = await fetch('https://dummyjson.com/products');
+    const data = await res.json();
+    setProducts(data.products);
+    console.log(data.products);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // const searchInput = e.currentTarget.elements[0] as HTMLInputElement;
+    setSearch(searchInput);
+    setSearchInput('');
+  }
+
+  const handleCategory = (category: string) => {
+    setCategories(prevState => (
+      prevState.includes(category) ? prevState.filter(cat => cat !== category) : [...prevState, category]
+    ))
+
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className='flex'>
+      <div className='p-2 border-r w-1/4'>
+        <form onSubmit={handleSubmit} className='flex justify-center items-center mt-4'>
+          <input type="text" value={searchInput} onChange={handleChange} placeholder='Search...' className='shadow-[0_0_1px] rounded-3xl w-100 py-2 pl-4' />
+          <button className='-translate-x-8'><Search /></button>
+        </form>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div>
+          <h2 className='text-3xl mt-4'>Categories</h2>
+          <div className='grid grid-cols-3 gap-4 mt-4'>
+            <div onClick={() => handleCategory('beauty')} className={`p-4 rounded-lg text-center hover:scale-110 transition ${categories.includes('beauty') ? 'bg-black text-white' : 'bg-gray-300'}`}>Beauty</div>
+            <div onClick={() => handleCategory('apparel')} className={`p-4 rounded-lg text-center hover:scale-110 transition ${categories.includes('apparel') ? 'bg-blue-500 text-white' : 'bg-gray-300'}`}>Apparel</div>
+            <div onClick={() => handleCategory('fragrances')} className={`p-4 rounded-lg text-center hover:scale-110 transition ${categories.includes('fragrances') ? 'bg-black text-white' : 'bg-gray-300'}`}>Fragrances</div>
+            <div onClick={() => handleCategory('gadget')} className={`p-4 rounded-lg text-center hover:scale-110 transition ${categories.includes('gadget') ? 'bg-black text-white' : 'bg-gray-300'}`}>Gadget</div>
+            <div onClick={() => handleCategory('kitchen')} className={`p-4 rounded-lg text-center hover:scale-110 transition ${categories.includes('kitchen') ? 'bg-black text-white' : 'bg-gray-300'}`}>Kitchen</div>
+            <div onClick={() => handleCategory('outdoors')} className={`p-4 rounded-lg text-center hover:scale-110 transition ${categories.includes('outdoors') ? 'bg-black text-white' : 'bg-gray-300'}`}>Outdoors</div>
+            <div onClick={() => handleCategory('furniture')} className={`p-4 rounded-lg text-center hover:scale-110 transition ${categories.includes('furniture') ? 'bg-black text-white' : 'bg-gray-300'}`}>Furniture</div>
+            <div onClick={() => handleCategory('books')} className={`p-4 rounded-lg text-center hover:scale-110 transition ${categories.includes('books') ? 'bg-black text-white' : 'bg-gray-300'}`}>Books</div>
+            <div onClick={() => handleCategory('groceries')} className={`p-4 rounded-lg text-center hover:scale-110 transition ${categories.includes('groceries') ? 'bg-black text-white' : 'bg-gray-300'}`}>Groceries</div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div className='grid grid-cols-4 gap-4 mt-4'>
+        {products.filter(product => product.title.toLowerCase().includes(search.toLowerCase()) && (categories.length === 0 || categories.some(category => product.category.toLowerCase().includes(category.toLowerCase())))).map((product) => (
+          <div key={product.id} className='border p-4'>
+            <img src={product.thumbnail} alt='no image' />
+            <h2>{product.title}</h2>
+            {/* <p>{product.description}</p> */}
+            <p>${product.price}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
+
+export default Home;
