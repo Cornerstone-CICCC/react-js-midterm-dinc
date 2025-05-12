750824a6848eed1e2cfabedce69b424e691bf740
Nana Okamoto <nakmtn07@gmail.com>
2025-05-12 13:24:24 -0700

    add: add post page

---
+++ frontend/src/app/(auth)/posts/new/page.tsx
@@ -0,0 +1,9 @@
+const PostsPage = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default PostsPage;
---
+++ frontend/src/schemas/artworkSchema.ts
@@ -0,0 +1,20 @@
+'use client';
+
+import { z } from 'zod';
+
+export const artworkSchema = z.object({
+  title: z
+    .string()
+    .nonempty('Title is required')
+    .min(3, 'Title must be 3-30 characters')
+    .max(30, 'Title must be 3-30 characters'),
+  description: z.string().max(150, 'Description must be under 150 characters'),
+  price: z.number().min(0, 'Price must be greater than 0'),
+  image: z
+    .array(z.string().url('Invalid image URL'))
+    .min(1, 'At least one image is required')
+    .max(5, 'Maximum 5 images allowed'),
+  category: z.string().nonempty('Category is required'),
+});
+
+export type ArtworkFormInputs = z.infer<typeof artworkSchema>;
