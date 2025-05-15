236b4a3aaf5ab4230f8171561a5e1dbbf0e55408
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-05-15 10:02:19 -0700

    fix: use router.push instead of redirect

--- frontend/src/app/(auth)/payment/page.tsx
+++ frontend/src/app/(auth)/payment/page.tsx
@@ -1,88 +1,133 @@
-"use client";
+'use client';
 
-import Inputpair from "@/components/payment/Inputpair";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
-import { redirect } from "next/navigation";
+import Inputpair from '@/components/payment/Inputpair';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const PaymentPage = () => {
+  const router = useRouter();
   const handleSubmitPayment = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    redirect("/payment/complete");
-  }
+    router.push('/payment/complete');
+  };
   return (
-
-      <div className="p-20">
-        <div className="items-start space-y-5 lg:grid lg:grid-cols-6 lg:space-x-4">
-          <div className="col-span-4 lg:px-4">
-            <div className="border-t-2 border-black">
-              <div className="flex flex-col justify-start items-start p-6 border-b-1 space-y-6">
-                <div className="flex items-center gap-4">
-                  <div className="w-full flex items-center space-x-4">
-                    <Avatar>
-                      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                      <AvatarFallback>S</AvatarFallback>
-                    </Avatar>
-                    <p className="truncate font-semibold">Seller name</p>
-                  </div>
+    <div className="p-20">
+      <div className="items-start space-y-5 lg:grid lg:grid-cols-6 lg:space-x-4">
+        <div className="col-span-4 lg:px-4">
+          <div className="border-t-2 border-black">
+            <div className="flex flex-col justify-start items-start p-6 border-b-1 space-y-6">
+              <div className="flex items-center gap-4">
+                <div className="w-full flex items-center space-x-4">
+                  <Avatar>
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>S</AvatarFallback>
+                  </Avatar>
+                  <p className="truncate font-semibold">Seller name</p>
                 </div>
-                <div className="flex justify-start items-start gap-4">
-                  <div className="size-30 shrink-0 bg-gray-200 overflow-hidden relative">
-                    <Image src="/tmp.png" alt="" className="object-cover" fill />
-                  </div>
-                  <div className="flex flex-col space-y-2">
-                    <p className="text-md truncate text-muted-foreground">ModernModern Modern Modern Wooden Coffee Table</p>
-                    <span className="font-bold text-lg">$140.00</span>
-                  </div>
+              </div>
+              <div className="flex justify-start items-start gap-4">
+                <div className="size-30 shrink-0 bg-gray-200 overflow-hidden relative">
+                  <Image src="/tmp.png" alt="" className="object-cover" fill />
+                </div>
+                <div className="flex flex-col space-y-2">
+                  <p className="text-md truncate text-muted-foreground">
+                    ModernModern Modern Modern Wooden Coffee Table
+                  </p>
+                  <span className="font-bold text-lg">$140.00</span>
                 </div>
               </div>
             </div>
-
-            <div className="mt-10 mb-10 lg:mt-20 pt-5 border-t-2 border-black">
-              <form className="space-y-6" onSubmit={handleSubmitPayment}>
-                <div>
-                  <h3 className="font-semibold text-md mb-4">Shipping Address</h3>
-                  <div className="space-y-5">
-                    <Inputpair label="Email" sup={true} id="email" type="email" placeholder="Enter your email" />
-                    <Inputpair label="Address" sup={true} id="address" type="text" placeholder="Enter your address" />
-                  </div>
-                </div>
-              </form>
-            </div>
           </div>
-          <div className="col-span-2">
-            <Card>
-              <CardHeader>
-                <CardTitle className="text-xl">Payment</CardTitle>
-                <CardDescription>Have this item in one-click.</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <form className="space-y-6" onSubmit={handleSubmitPayment}>
-                  <div>
-                    <h3 className="font-semibold text-md mb-4">Payment Method</h3>
-                    <div>
-                      <div className="space-y-5">
-                        <Inputpair label="Card Number" sup={true} id="cardNumber" type="number" placeholder="Enter your card number" />
-                        <Inputpair label="Expiration Date" sup={true} id="exprDate" type="text" placeholder="MM / YY" />
-                        <Inputpair label="CVC" sup={true} id="cvc" type="number" placeholder="XXX" />
-                        <Inputpair label="Card Holder Name" sup={true} id="holderName" type="text" placeholder="Enter card holder name" />
-                      </div>
-                    </div>
-                  </div>
-                  <Button size={"lg"} className="w-full">
-                    Pay with Card
-                  </Button>
-                </form>
-              </CardContent>
-            </Card>
+
+          <div className="mt-10 mb-10 lg:mt-20 pt-5 border-t-2 border-black">
+            <form className="space-y-6" onSubmit={handleSubmitPayment}>
+              <div>
+                <h3 className="font-semibold text-md mb-4">Shipping Address</h3>
+                <div className="space-y-5">
+                  <Inputpair
+                    label="Email"
+                    sup={true}
+                    id="email"
+                    type="email"
+                    placeholder="Enter your email"
+                  />
+                  <Inputpair
+                    label="Address"
+                    sup={true}
+                    id="address"
+                    type="text"
+                    placeholder="Enter your address"
+                  />
+                </div>
+              </div>
+            </form>
           </div>
         </div>
+        <div className="col-span-2">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Payment</CardTitle>
+              <CardDescription>Have this item in one-click.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form className="space-y-6" onSubmit={handleSubmitPayment}>
+                <div>
+                  <h3 className="font-semibold text-md mb-4">Payment Method</h3>
+                  <div>
+                    <div className="space-y-5">
+                      <Inputpair
+                        label="Card Number"
+                        sup={true}
+                        id="cardNumber"
+                        type="number"
+                        placeholder="Enter your card number"
+                      />
+                      <Inputpair
+                        label="Expiration Date"
+                        sup={true}
+                        id="exprDate"
+                        type="text"
+                        placeholder="MM / YY"
+                      />
+                      <Inputpair
+                        label="CVC"
+                        sup={true}
+                        id="cvc"
+                        type="number"
+                        placeholder="XXX"
+                      />
+                      <Inputpair
+                        label="Card Holder Name"
+                        sup={true}
+                        id="holderName"
+                        type="text"
+                        placeholder="Enter card holder name"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <Button size={'lg'} className="w-full">
+                  Pay with Card
+                </Button>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
       </div>
+    </div>
   );
 };
 
 export default PaymentPage;
-
-
