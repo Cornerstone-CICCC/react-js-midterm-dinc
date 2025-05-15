801f93750e0341688e82370556746d8f43ed06a9
Daz <shoeshinesamurai@gmail.com>
2025-05-15 04:36:44 -0700

    basic auth backend + user api

--- backend/src/types/user.d.ts
+++ backend/src/types/user.d.ts
@@ -1,1 +1,15 @@
-export interface IUser {}
+import mongoose from 'mongoose';
+
+export interface IUser extends mongoose.Document {
+  name?: string;
+  userName?: string;
+  email: string;
+  password: string;
+  bio?: string;
+  fileId?: string;
+  lastLogin?: Date | null;
+  isLoggedIn?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+  comparePassword(password: string): Promise<boolean>;
+}
