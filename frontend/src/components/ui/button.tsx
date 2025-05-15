e5d768e790f1c6e1afa74ac11bc7be33e3bd7506
Daz <shoeshinesamurai@gmail.com>
2025-05-15 11:49:45 -0700

    fix ui files to resolve conflict

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -20,8 +20,6 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        tertiary:
-          "bg-goldenrod-yellow hover:bg-goldenrod-yellow/90",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
