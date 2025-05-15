0332afb81711943ff5289a2e0c9f6cc491ff3689
Tomoya <flyingfox193807@gmail.com>
2025-05-15 09:43:44 -0700

    deleted detail page and modified style

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,7 +9,7 @@ type Props = {
   children: React.ReactNode;
 }
 
-const HomeChild = ({ children }: Props) => {
+const HomeChild = () => {
 
   const { search, setSearch, searchInput, setSearchInput, categories, handleCategory, handleResetFilter } = useSearchContext();
 
@@ -26,15 +26,14 @@ const HomeChild = ({ children }: Props) => {
     fetchProducts();
   }, []);
 
-
   return (
     <SearchLayout>
       <div className='grid grid-cols-4 gap-4 mt-20 w-full p-4'>
         {products.filter(product => product.title.toLowerCase().includes(search.toLowerCase()) && (categories.length === 0 || categories.some(category => product.category.toLowerCase().includes(category.toLowerCase())))).map((product) => (
           <Link href={`/products/${product.id}`} key={product.id}>
-            <div className='border p-4'>
+            <div className='border p-4 min-h-[270px] shadow-md rounded-md hover:scale-105 transition'>
               <img src={product.thumbnail || 'https://placehold.co/200x150'} alt='no image' />
-              <h2>{product.title}</h2>
+              <h2 className='line-clamp-2 text-sm text-gray-500 mb-2'>{product.title}</h2>
               <p>${product.price}</p>
             </div>
           </Link>
@@ -44,12 +43,10 @@ const HomeChild = ({ children }: Props) => {
   )
 }
 
-const Home = ({ children }: Props) => {
+const Home = () => {
   return (
     <SearchProvider>
-      <HomeChild>
-        {children}
-      </HomeChild>
+      <HomeChild />
     </SearchProvider>
   )
 }
--- frontend/src/components/SearchLayout.tsx
+++ frontend/src/components/SearchLayout.tsx
@@ -38,27 +38,26 @@ const SearchLayout = ({ children }: Props) => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setSearch(searchInput);
-    setSearchInput('');
   }
 
   return (
     <div className='flex p-4 w-full'>
-      <div className='p-2 border-r w-2/5'>
+      <div className='p-2 border-r w-1/4'>
         <form onSubmit={handleSubmit} className='flex justify-center items-center mt-4'>
           <input type="text" value={searchInput} onChange={handleChange} placeholder='Search...' className='shadow-[0_0_1px] rounded-3xl w-full py-2 pl-4 pr-9' />
           <button className='-translate-x-8 cursor-pointer'><Search /></button>
         </form>
 
         <div>
-          <h2 className='text-3xl mt-4'>Categories</h2>
-          <div className='grid grid-cols-3 gap-4 mt-4'>
+          <div className='flex justify-between pr-4 my-6'>
+            <h2 className='text-2xl'>Categories</h2>
+            <button onClick={handleResetFilter} className='py-2 px-4 bg-gray-300 rounded-lg cursor-pointer hover:scale-110 transition'>Reset</button>
+          </div>
+          <div className='grid grid-cols-2 gap-4 mt-4'>
             {categoriesList.map((category) => (
-              <div key={category} onClick={() => handleCategory(category)} className={`p-4 rounded-lg text-center cursor-pointer hover:scale-110 transition ${categories.includes(category) ? 'bg-black text-white' : 'bg-gray-300'}`}>{category.charAt(0).toUpperCase() + category.slice(1)}
+              <div key={category} onClick={() => handleCategory(category)} className={`p-4 rounded-lg cursor-pointer max-w-[150px] hover:scale-110 transition ${categories.includes(category) ? 'bg-black text-white' : 'bg-gray-300'}`}>{category.charAt(0).toUpperCase() + category.slice(1)}
               </div>
             ))}
-          </div>
-          <div>
-            <button onClick={handleResetFilter} className='py-2 px-4 bg-gray-300 rounded-lg mt-6 cursor-pointer hover:scale-110 transition'>Reset</button>
           </div>
         </div>
       </div>
@@ -68,17 +67,5 @@ const SearchLayout = ({ children }: Props) => {
     </div>
   )
 }
-
-// const SearchLayout = ({ children }: Props) => {
-//   return (
-//     <div className='flex p-4 w-full'>
-//       <SearchProvider>
-//         <SearchLayoutChild>
-//           {children}
-//         </SearchLayoutChild>
-//       </SearchProvider>
-//     </div>
-//   )
-// }
 
 export default SearchLayout;
