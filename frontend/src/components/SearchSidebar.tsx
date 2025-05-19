a98c52d66d9510e2052a3279670ecda5d55c7359
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-05-19 01:05:36 -0700

    fix: home page design

--- frontend/src/components/SearchSidebar.tsx
+++ frontend/src/components/SearchSidebar.tsx
@@ -1,52 +1,51 @@
 'use client';
-import { Search } from 'lucide-react';
-import { useSearchContext } from '@/context/SearchContext';
+import { SearchIcon } from 'lucide-react';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { CATEGORIES } from '@/constants/categories';
+import { useState } from 'react';
 
 const SearchSidebar = () => {
-
-  const {
-    searchInput,
-    setSearchInput,
-    selectedCategory,
-    handleCategory,
-  } = useSearchContext();
-
-  const categoriesList = [
-    'beauty',
-    'apparel',
-    'fragrances',
-    'gadget',
-    'kitchen',
-    'outdoors',
-    'furniture',
-    'books',
-    'groceries'
-  ]
+  const [searchValue, setSearchValue] = useState<string>('');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchInput(e.target.value);
-  }
+    setSearchValue(e.target.value);
+  };
 
   return (
-    <div className='px-4 md:w-64 fixed z-100 bg-white max-md:w-full transition h-auto'>
-      <form method='GET' action='/products' className='flex justify-center items-center md:mt-4'>
-        <input type="text" value={searchInput} name='search' onChange={handleChange} placeholder='Search...' className='shadow-[0_0_1px] rounded-3xl w-full py-2 pl-4 pr-9' />
-        <button className='-translate-x-8 cursor-pointer'><Search /></button>
-      </form>
+    <div className="md:w-60 fixed z-5 bg-white max-md:w-full transition h-auto pr-5">
+      <div className="flex justify-between gap-1">
+        <Input
+          placeholder="Search..."
+          value={searchValue}
+          className="p-4"
+          name="search"
+          onChange={handleChange}
+        />
+        <Button size={'icon'} variant={'secondary'}>
+          <SearchIcon />
+        </Button>
+      </div>
 
       <div>
-        <div className='flex justify-between pr-4 md:my-4 max-md:my-2'>
-          <h2 className='text-2xl'>Categories</h2>
+        <div className="flex justify-between pr-4 md:my-4 max-md:my-2">
+          <h2 className="text-2xl">Categories</h2>
         </div>
-        <div className='md:grid grid-cols-2 gap-4 mt-4 flex max-md:overflow-scroll max-md:pb-4'>
-          {categoriesList.map((category) => (
-            <div key={category} onClick={() => handleCategory(category)} className={`p-4 rounded-lg cursor-pointer max-w-[150px] hover:scale-110 transition ${selectedCategory === category ? 'bg-black text-white' : 'bg-gray-300'}`}>{category.charAt(0).toUpperCase() + category.slice(1)}
-            </div>
+        <div className="md:grid grid-cols-2 gap-4 mt-4 flex max-md:overflow-scroll max-md:pb-4">
+          {CATEGORIES.map((cate) => (
+            <Button
+              key={cate}
+              variant={'secondary'}
+              size={'sm'}
+              className="text-xs truncate"
+            >
+              {cate}
+            </Button>
           ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SearchSidebar;
