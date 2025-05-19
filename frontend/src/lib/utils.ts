3c374cd83e88cee099695da948c5b717117e0959
iki789 <ikhlak789@gmail.com>
2025-05-18 23:22:11 -0700

    updated productUpdated and added slug utils

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function slugToTitle(slug: string) {
+  const words = slug.split('-');
+  const title = words
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+  return title;
+}
+
+export function tilteToSlug(slug: string) {
+  const words = slug.split(' ');
+  const title = words
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join('-');
+  return title;
+}
